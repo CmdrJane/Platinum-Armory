5972aa42787b0ea3bc6b5066c3e63b63315fed8f
CmdrJane <48736985+CmdrJane@users.noreply.github.com>
2021-04-03 07:23:53 +0300

    init commit

--- src/main/java/ru/aiefu/platinumarmory/PlatinumArmory.java
+++ src/main/java/ru/aiefu/platinumarmory/PlatinumArmory.java
@@ -4,8 +4,13 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+import net.minecraft.util.registry.Registry;
 
 public class PlatinumArmory implements ModInitializer {
+
+	public static final String MOD_ID = "platinumarmory";
 
 	public static final Item PLATINUM_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 	public static final Item PLATINUM_GOLDEN_ALLOY_RAW = new Item(new FabricItemSettings().group(ItemGroup.MISC));
@@ -18,10 +23,15 @@ public class PlatinumArmory implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, craftID("platinum_ingot"), PLATINUM_INGOT);
+		Registry.register(Registry.ITEM, craftID("plat_gold_allow_raw"), PLATINUM_GOLDEN_ALLOY_RAW);
+		Registry.register(Registry.ITEM, craftID("plat_gold_allow_smelted"), PLATINUM_GOLDEN_ALLOY_SMELTED);
+		Registry.register(Registry.ITEM, craftID("diamond_plate"), DIAMOND_PLATE);
+		Registry.register(Registry.ITEM, craftID("iridium_ingot"), IRIDIUM_INGOT);
+		Registry.register(Registry.ITEM, craftID("plat_ir_allow_raw"), PLATINUM_IRIDIUM_ALLOY_RAW);
+		Registry.register(Registry.ITEM, craftID("plat_ir_allow_smelted"), PLATINUM_IRIDIUM_ALLOY_SMELTED);
+	}
+	public Identifier craftID(String id){
+		return new Identifier(MOD_ID, id);
 	}
 }
